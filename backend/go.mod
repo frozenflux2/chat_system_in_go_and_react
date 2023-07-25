d578588ef66db8130d4a94a062bd4c4867e4a2ee
lumi518 <aurora518star.com@gmail.com>
2023-07-24 23:23:04 -0700

    first commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/lumi518/chat_system_in_go_and_react
+
+go 1.20
---
+++ go_start/helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = map[int]int{1: 1, 2: 2}
+	b := map[string]int{"1": 1, "2": 2}
+
+	fmt.Println(a, b)
+}
